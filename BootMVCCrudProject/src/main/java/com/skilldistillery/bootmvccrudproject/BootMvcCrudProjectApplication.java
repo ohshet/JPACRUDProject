b4c5c8a2492f5f7b5a4c31d4132900b29ac5cc90
John Overberg <john.richard.overberg@gmail.com>
2019-03-01 16:01:00 -0700

    connected boot and jpa projects

---
+++ BootMVCCrudProject/src/main/java/com/skilldistillery/bootmvccrudproject/BootMvcCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.bootmvccrudproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcCrudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcCrudProjectApplication.class, args);
+	}
+
+}
